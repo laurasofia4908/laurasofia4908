d188692c505fd9ac6c6d1cec6d85088227388252
laurasofia4908 <135129067+laurasofia4908@users.noreply.github.com>
2023-05-31 08:20:29 -0500

    Update Sum_Python.py

--- Sum_Python.py
+++ Sum_Python.py
@@ -1,24 +1,32 @@
-class Receta:
-    def __init__(self, nombre, ingredientes, pasos):
-        self.nombre = nombre
-        self.ingredientes = ingredientes
-        self.pasos = pasos
+# Diccionario de recetas
+recetas = {
+    "panqueques": {
+        "ingredientes": ["harina", "azúcar", "leche", "huevos"],
+        "pasos": ["Mezclar los ingredientes secos en un tazón.", "Agregar la leche y los huevos.", "Mezclar bien hasta obtener una masa suave.", "Calentar una sartén y verter la masa.", "Cocinar por ambos lados hasta que estén dorados."]
+    },
+    "ensalada": {
+        "ingredientes": ["lechuga", "tomate", "cebolla", "pepino"],
+        "pasos": ["Lavar y cortar los ingredientes.", "Mezclar todo en un tazón.", "Aliñar con aceite, vinagre, sal y pimienta al gusto."]
+    },
+    "pasta": {
+        "ingredientes": ["pasta", "salsa de tomate", "cebolla", "ajo"],
+        "pasos": ["Cocinar la pasta según las instrucciones del paquete.", "En una sartén, sofreír la cebolla y el ajo.", "Agregar la salsa de tomate y cocinar a fuego lento.", "Escurrir la pasta y mezclar con la salsa.", "Servir caliente."]
+    }
+}
 
-    def imprimir_receta(self):
-        print("Receta: {}".format(self.nombre))
+# Función para mostrar una receta
+def mostrar_receta(nombre):
+    receta = recetas.get(nombre)
+    if receta:
+        print("Receta de", nombre.capitalize())
         print("Ingredientes:")
-        for ingrediente in self.ingredientes:
-            print("- {}".format(ingrediente))
+        for ingrediente in receta["ingredientes"]:
+            print("- ", ingrediente)
         print("Pasos:")
-        for i, paso in enumerate(self.pasos, 1):
-            print("{}. {}".format(i, paso))
+        for paso in receta["pasos"]:
+            print("- ", paso)
+    else:
+        print("No se encontró la receta", nombre)
 
 # Ejemplo de uso
-receta_pizza = Receta("Pizza", ["Masa de pizza", "Salsa de tomate", "Queso", "Pepperoni"], 
-                      ["Precalienta el horno a 200 grados Celsius.",
-                       "Extiende la masa de pizza sobre una bandeja para horno.",
-                       "Unta la salsa de tomate sobre la masa.",
-                       "Espolvorea el queso y distribuye el pepperoni sobre la pizza.",
-                       "Hornea durante 15-20 minutos o hasta que la masa esté dorada y el queso derretido."])
-
-receta_pizza.imprimir_receta()
+mostrar_receta("panqueques")
