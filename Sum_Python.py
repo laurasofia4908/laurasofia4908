10dc0e8b2f8b3f1490ab49cd72eeeaac07e55344
laurasofia4908 <135129067+laurasofia4908@users.noreply.github.com>
2023-05-31 08:11:23 -0500

    Update Sum_Python.py

--- Sum_Python.py
+++ Sum_Python.py
@@ -1,10 +1,16 @@
-palabras = ["lápiz", "león", "luna", "libro", "largo", "luz", "lindo", "leche", "lugar"]
+class Palabras:
+    def __init__(self):
+        self.palabras = ["taza", "tigre", "tomate", "tren", "televisión", "tarjeta", "teléfono", "tiempo", "tesoro"]
+
+    def palabras_con_letra(self, letra):
+        return [palabra for palabra in self.palabras if palabra.startswith(letra)]
+
 
 letra = input("Ingresa una letra: ")
 
-palabras_con_letra = [palabra for palabra in palabras if palabra.startswith(letra)]
+objeto_palabras = Palabras()
+palabras_con_letra_t = objeto_palabras.palabras_con_letra(letra)
 
 print("Palabras que comienzan con la letra", letra + ":")
-
-for palabra in palabras_con_letra:
+for palabra in palabras_con_letra_t:
     print(palabra)
