b3b9573ba752dd253775c4ed7ce0554522ea8fca
laurasofia4908 <135129067+laurasofia4908@users.noreply.github.com>
2023-05-31 08:16:28 -0500

    Update Sum_Python.py

--- Sum_Python.py
+++ Sum_Python.py
@@ -1,7 +1,24 @@
-animales = ["perro", "gato", "pato", "pingüino", "pulpo", "puma", "paloma", "pájaro", "pez"]
+class Receta:
+    def __init__(self, nombre, ingredientes, pasos):
+        self.nombre = nombre
+        self.ingredientes = ingredientes
+        self.pasos = pasos
 
-animales_con_p = [animal for animal in animales if animal.startswith("p")]
+    def imprimir_receta(self):
+        print("Receta: {}".format(self.nombre))
+        print("Ingredientes:")
+        for ingrediente in self.ingredientes:
+            print("- {}".format(ingrediente))
+        print("Pasos:")
+        for i, paso in enumerate(self.pasos, 1):
+            print("{}. {}".format(i, paso))
 
-print("Animales que comienzan con la letra P:")
-for animal in animales_con_p:
-    print(animal)
+# Ejemplo de uso
+receta_pizza = Receta("Pizza", ["Masa de pizza", "Salsa de tomate", "Queso", "Pepperoni"], 
+                      ["Precalienta el horno a 200 grados Celsius.",
+                       "Extiende la masa de pizza sobre una bandeja para horno.",
+                       "Unta la salsa de tomate sobre la masa.",
+                       "Espolvorea el queso y distribuye el pepperoni sobre la pizza.",
+                       "Hornea durante 15-20 minutos o hasta que la masa esté dorada y el queso derretido."])
+
+receta_pizza.imprimir_receta()
